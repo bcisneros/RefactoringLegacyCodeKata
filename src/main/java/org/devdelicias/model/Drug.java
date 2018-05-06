fe59d6e1977e0df663cc2286a13ba60c93b15ffb
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-05 20:33:49 -0600

    More preparation for katas

--- src/main/java/org/devdelicias/model/Drug.java
+++ src/main/java/org/devdelicias/model/Drug.java
@@ -1,12 +1,19 @@
 package org.devdelicias.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Drug {
     private final Long _id;
     private final String _name;
+    private List<DrugIngredient> ingredients;
 
     public Drug(Long id, String name) {
         _id = id;
         _name = name;
+        ingredients = new ArrayList<>();
     }
 
     public Long id() {
@@ -15,5 +22,13 @@ public class Drug {
 
     public String name() {
         return _name;
+    }
+
+    public void add(DrugIngredient... drugIngredients) {
+        this.ingredients.addAll(Arrays.asList(drugIngredients));
+    }
+
+    public List<DrugIngredient> ingredients() {
+        return Collections.unmodifiableList(this.ingredients);
     }
 }
