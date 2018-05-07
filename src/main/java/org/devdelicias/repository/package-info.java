ba68f1e25b37d20dffe4ea477fdcee0e4819e364
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-07 14:24:58 -0600

    Add MIT License

--- src/main/java/org/devdelicias/repository/package-info.java
+++ src/main/java/org/devdelicias/repository/package-info.java
@@ -21,25 +21,4 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package org.devdelicias;
-
-import org.devdelicias.model.Drug;
-import org.devdelicias.model.Patient;
-import org.devdelicias.service.DispenseDrugException;
-import org.devdelicias.service.DispenseDrugService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Main {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-    private static final long PATIENT_ID = 159L;
-
-    public static void main(String[] args) throws DispenseDrugException {
-        LOGGER.info("Running DispenseDrugService");
-        new DispenseDrugService().dispenseDrugToPatient(
-            new Drug(1L, "Xanax"),
-            new Patient(PATIENT_ID, "Steven")
-        );
-    }
-}
+package org.devdelicias.repository;
