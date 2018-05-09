dc33d3ce4c75e3dee4bdacfa07ec7222c7278573
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-05-08 21:19:05 -0600

    Add second test: Expired ingredients

--- src/test/java/org/devdelicias/service/DispenseDrugServiceTest.java
+++ src/test/java/org/devdelicias/service/DispenseDrugServiceTest.java
@@ -1,9 +1,11 @@
 package org.devdelicias.service;
 
+import java.util.Date;
 import java.util.List;
 import org.devdelicias.model.Drug;
 import org.devdelicias.model.DrugIngredient;
 import org.devdelicias.model.Patient;
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
 public class DispenseDrugServiceTest {
@@ -16,6 +18,18 @@ public class DispenseDrugServiceTest {
         Drug drug = new Drug(1L, "Drug Name");
 
         service.dispenseDrugToPatient(drug, UNUSED_PATIENT);
+    }
+
+    @Test(expected = DispenseDrugException.class)
+    public void cantDispenseDrugIfAnyIngredientIsExpired() throws DispenseDrugException {
+        DispenseDrugService service = new TestableDispenseDrugService();
+        Drug drug = new Drug(1L, "Drug Name");
+        Date yesterday = LocalDate.parse("2018-05-07").toDate();
+        DrugIngredient expiredIngredient = new DrugIngredient(1L, "Expired Ing.", yesterday);
+        drug.add(expiredIngredient);
+        Patient patient = null;
+
+        service.dispenseDrugToPatient(drug, patient);
     }
 
     private class TestableDispenseDrugService extends DispenseDrugService {
